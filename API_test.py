6d0d8299cee3371bf9efe71c04cc1949db15555d
eliz-sa <elisazhuang22@gmail.com>
2024-12-23 08:09:19 +0100

    fetch lines bu transport mode

--- API_test.py
+++ API_test.py
@@ -3,24 +3,19 @@ import json
 
 def make_api_request(url, params=None, headers=None):
     # Send the GET request to the API
-    res = requests.get(url, headers=headers, params=params)
 
-    # Check if the request was successful
-    if res.status_code == 200:
-        # Parse the JSON response if the status code is OK
-        data = res.json()
-        print('JSON Response:', data)
-        
+    try:
+        res = requests.get(url, headers=headers, params=params)
+        res.raise_for_status()  # Raise an exception for HTTP errors
+        data = res.json()        
         #for test
         with open('Reponse.json', 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)  # Write JSON with indentation
-
-        return data  # Return the parsed data
-    else:
-        print(f"Error {res.status_code}: {res.text}")
+        return data 
+    except requests.exceptions.RequestException as e:
+        print(f"Error during the request: {e}")
         return None
 
-# Example usage of this function for the IDFM API
 def fetch_lines_from_api(count=2, api_key='Wthw9UFyhoc3uoCss4mJ2QpWwi51X8kY'):
     # URL of the API
     url = 'https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia/lines'
@@ -36,5 +31,27 @@ def fetch_lines_from_api(count=2, api_key='Wthw9UFyhoc3uoCss4mJ2QpWwi51X8kY'):
     # Use the generic make_api_request function
     return make_api_request(url, params=params, headers=headers)
 
+def get_lines_by_transport_mode(physical_mode, api_key='Wthw9UFyhoc3uoCss4mJ2QpWwi51X8kY'):
+    base_url = 'https://prim.iledefrance-mobilites.fr/marketplace/v2/navitia'
+    
+    # Ensure that the physical mode is a valid one
+    valid_modes = ["Bike", "BikeSharingService", "Bus", "Car", "Funicular", "Metro", 
+                   "RailShuttle", "RapidTransit", "Train", "LocalTrain", "Tramway"]
+    
+    if physical_mode not in valid_modes:
+        print(f"Invalid physical mode: {physical_mode}")
+        return None
+
+    # Construct the URL with the given parameters
+    url = f"{base_url}/line_reports/physical_modes/physical_mode%3A{physical_mode}/lines"
+
+    # Set the request headers with the provided API key
+    headers = {'Accept': 'application/json', 'apikey': api_key}
+    
+    # Make the API request using the helper function
+    return make_api_request(url, headers=headers)
+
 # Example of calling the function
-result = fetch_lines_from_api(2)
+result = get_lines_by_transport_mode("Metro")
+
+print(result['lines'])
